f83234d376d980964bab53c3202ebc0fbd03ac11
Tim Dettmar <beanfacts@protonmail.com>
2026-04-18 15:43:46 +0200

    increase number of write slots During operation it seems 6 write contexts are insufficient.

--- lgmp/src/modules/fabric/nfr/nfr_resource.h
+++ lgmp/src/modules/fabric/nfr/nfr_resource.h
@@ -396,9 +396,9 @@ int nfrPrintCQError(int logLevel, const char * func, const char * file,
 inline static struct NFRCommBufInfo nfrGetDefaultCommBufInfo(void)
 {
   struct NFRCommBufInfo info = {0};
-  info.txSlots               = 60;
-  info.rxSlots               = 60;
-  info.writeSlots            = 6;
+  info.txSlots               = 40;
+  info.rxSlots               = 40;
+  info.writeSlots            = 46;
   info.ackSlots              = 2;
   info.slotSize              = NETFR_MESSAGE_MAX_SIZE;
   assert(NFR_TOTAL_SLOTS(info) == NETFR_TOTAL_CONTEXT_COUNT);
